b8a99a804c2d22850d277cfca747b89ed28c5577
breno-aredes <breno_mg10@hotmail.com>
2022-11-24 10:51:26 -0300

    v 0.3.1 coloca a palavra aleatoria

--- src/Jogo.js
+++ src/Jogo.js
@@ -1,15 +1,19 @@
 // (imagem da forca, botão de iniciar, palavra do jogo)
 import palavras from "./palavras";
 
-const randomWord = palavras[Math.floor(Math.random() * palavras.length)]
+//pega uma palavra aleatoria e transforma em lista
+const randomWord = palavras[Math.floor(Math.random() * palavras.length)].split('')
 
 export default function Game() {
+
 
     return (
         <>
             <img src="img/forca0.png" alt="" />
             <button class="chooseWord">Sortear Palavra</button>
-            <div className="randomWord">_________</div>
+            <div className="randomWord">
+                {randomWord.map((p) => `${p}`)}
+            </div>
         </>
     )
 }
